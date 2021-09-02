f69c052c5c8ffba3dc0106d0172e2ac1839864cf
Maciek <s17245@pjwstk.edu.pl>
2021-09-02 07:33:59 +0200

    Add method, no results, to be fixed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,20 @@ import java.sql.SQLOutput;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int x = 5;
+        myMethod(x);
+    }
+
+    static void myMethod(int n) {
+        int num1 = 0;
+        int counter = 0;
+        int num2 = 1;
+
+        while (counter > n) {
+            int num3 = num2 + num1;
+            num1 = num2;
+            num2 = num3;
+            counter = counter + 1;
+        }
     }
 }
