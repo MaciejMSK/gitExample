4fbb9238713fbb31576b5058e77a9a5f81063f7c
Maciek <s17245@pjwstk.edu.pl>
2021-11-28 19:24:25 +0100

    Add simply unit test

---
+++ src/com/company/MyMethod.java
@@ -0,0 +1,10 @@
+package com.company;
+
+class MyMethod {
+
+    static int printAsumB(int a, int b){
+        int sum = a + b;
+        System.out.println("\n moja suma to: " + sum);
+        return sum;
+    }
+}
